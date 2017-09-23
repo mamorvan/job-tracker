99894dca1b3c66dbba7e8cf04e765bae52a3a837
Mary Morvan <mamorvan@yahoo.com>
2017-09-23 13:06:59 -0400

    run webpack :pray:

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1>Who Wants to Hire Mary?</h1>
+                </header>
+            </div>
+        )
+    }
+};
+
+export default Main;
